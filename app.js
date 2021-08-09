a836ab8c8f762fe1e2794d316a2db9d7a5373ad9
vickiespindola <ruviespindola@gmail.com>
2021-08-09 16:28:50 -0300

    modifiqué app.js

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 
-app.listen(port, () => console.log('Servidor corriendo en https://localhost:' + port));
+app.listen(port, () => console.log('Servidor corriendo en http://localhost:' + port));
